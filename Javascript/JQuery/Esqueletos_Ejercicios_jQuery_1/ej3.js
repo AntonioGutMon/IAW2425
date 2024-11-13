4992bbc3cd68fb418058a78838cdd43e195a4e99
AntonioGutMon <antoniogutierrez05@iesamachado.org>
2024-11-13 13:34:29 +0100

    a

---
+++ Javascript/JQuery/Esqueletos_Ejercicios_jQuery_1/ej3.js
@@ -0,0 +1,12 @@
+/* Rellena este fichero */
+$(document).ready(function () {
+    $("#btn-aumentar").click(function(){
+        $(".pares, h1").css({"font-size": "250%", "color": "red"})
+    })
+});
+
+$(document).ready(function () {
+    $("#btn-disminuir").click(function(){
+        $(".pares, h1").css({"font-size": "60%", "color": "green"})
+    })
+});
