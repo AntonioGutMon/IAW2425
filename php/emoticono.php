9edbad93f112600daefa4ee19743a7324f50f888
AntonioGutMon <antoniogutierrez05@iesamachado.org>
2024-12-02 12:06:51 +0100

    a

---
+++ php/emoticono.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emoticono</title>
+</head>
+<body>
+    <?php
+        $numero = rand(128512,128586);
+    ?>
+    <p>I will display &#<?= $numero ?></p>
+</body>
+</html>
---
+++ php/imagenesAleatorias/imagenesAleatorias.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imagen Aleatoria</title>
+    <style>
+        div{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        img {
+            width: 240px;
+            height: 240px;
+        }
+        h1{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
+</head>
+<body>
+<?php 
+    $array = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p");
+    $random = rand(0,15);
+    $tripulante = $array[$random];
+?>
+    <h1>Bleach girls:</h1><div>
+    <img src="<?= $tripulante; ?>.jpeg" alt=""></div>
+
+</body>
+</html>
