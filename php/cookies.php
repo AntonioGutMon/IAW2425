252313da00af8b8b7a58b1b7f611dd23dba4ae9a
AntonioGutMon <antoniogutierrez05@iesamachado.org>
2024-12-19 14:15:14 +0100

    a

---
+++ php/cookies.php
@@ -0,0 +1,28 @@
+<?php
+    if ( isset( $_COOKIE[ 'visitas' ] ) ) {
+
+        setcookie( 'visitas', $_COOKIE[ 'visitas' ] + 1, time() + 3600 * 24 );
+        setcookie("lang", $_SERVER['HTTP_ACCEPT_LANGUAGE'], time() + 3600 * 24);
+        $visita = 'Numero de visitas: '.$_COOKIE[ 'visitas' ];
+        $idioma = 'Estas usando el idioma: ' . $_COOKIE['lang'];
+    }
+    else {
+    
+        setcookie( 'visitas', 1, time() + 3600 * 24 );
+        $visita = 'Bienvenido por primera vez a nuesta web';
+        $idioma = 'Bienvenido por primera vez, estas usando el idioma: ' . $_COOKIE['lang'];
+    }
+?>
+<html>
+    <head>
+        <title>Cookie</title>
+    </head>
+    <body>
+        <p>
+            <?php 
+            echo $visita . "<br>";
+            echo $idioma;
+            ?>
+        </p>    
+    </body>
+</html> 
