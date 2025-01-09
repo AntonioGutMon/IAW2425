acc00114071939b2503ab51d50d3a3f09845b986
AntonioGutMon <antoniogutierrez05@iesamachado.org>
2025-01-09 14:25:50 +0100

    a

--- php/cookiesegura.php
+++ php/cookiesegura.php
@@ -1,14 +1,14 @@
 <?php
     if ( isset( $_COOKIE[ 'visitas' ] ) ) {
 
-        setcookie( 'visitas', $_COOKIE[ 'visitas' ] + 1, time() + 3600 * 24);
-        setcookie("lang", $_SERVER['HTTP_ACCEPT_LANGUAGE'], time() + 3600 * 24);
+        setcookie( 'visitas', $_COOKIE[ 'visitas' ] + 1, time() + 3600 * 24, httponly:true);
+        setcookie("lang", $_SERVER['HTTP_ACCEPT_LANGUAGE'], time() + 3600 * 24, httponly:true);
         $visita = 'Numero de visitas: '.$_COOKIE[ 'visitas' ];
         $idioma = 'Estas usando el idioma: ' . $_COOKIE['lang'];
     }
     else {
     
-        setcookie( 'visitas', 1, time() + 3600 * 24 );
+        setcookie( 'visitas', 1, time() + 3600 * 24, httponly:true);
         $visita = 'Bienvenido por primera vez a nuesta web';
         $idioma = 'Bienvenido por primera vez, estas usando el idioma: ' . $_COOKIE['lang'];
     }
